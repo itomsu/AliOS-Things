cd952439162a89e51235abd7ea0583044f9850f2
Tom Su <tomsu@raindi.com.cn>
2019-01-28 14:21:01 +0800

    set stdio_uart to 1.

--- board/developerkit/aos/board.h
+++ board/developerkit/aos/board.h
@@ -2,9 +2,9 @@
 #define MODEL               "STM32L4"
 
 #ifdef BOOTLOADER
-#define STDIO_UART 0
+#define STDIO_UART 1
 #define STDIO_UART_BUADRATE 115200
 #else
-#define STDIO_UART 0
+#define STDIO_UART 1
 #define STDIO_UART_BUADRATE 115200
 #endif
